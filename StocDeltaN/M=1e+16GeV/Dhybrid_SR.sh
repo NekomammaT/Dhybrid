24a3a3141396b68d8d287468e2da87aa1cb3c66e
Yuichiro Tada <yuichiro.t.tada@gmail.com>
2023-04-29 16:18:09 +0900

    2000 1000

--- StocDeltaN/M=1e+16GeV/Dhybrid_SR.sh
+++ StocDeltaN/M=1e+16GeV/Dhybrid_SR.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #rm -f data/maxPdata.dat
-#while read line
-#do
-#    ./Dhybrid_SR $line
-#done < ./EoI_5th.dat
+while read line
+do
+    ./Dhybrid_SR $line
+done < ./EoI_2nd.dat
 
 #rm -f data/maxPdata.dat
-./Dhybrid_SR 2000 1000 1.124616056335486036962e-15
+#./Dhybrid_SR 2000 1000 1.124616056335486036962e-15
 
 
